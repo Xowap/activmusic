561121435e05712a9bb12a6307525a4774d371d5
Rémy Sanchez <remy.sanchez@hyperthese.net>
2015-05-14 15:28:55 +0200

    No AWS in prod

--- activmusic/settings/prod.py
+++ activmusic/settings/prod.py
@@ -3,22 +3,4 @@
 # activmusic
 # (c) 2014 ActivKonnect
 
-from .common import *
-from os import getenv
-
 ALLOWED_HOSTS = ['*']
-
-DEFAULT_FILE_STORAGE = 'activmusic.lib.storage.s3boto.S3BotoStorage'
-
-AWS_ACCESS_KEY_ID = getenv('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = getenv('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = getenv('AWS_STORAGE_BUCKET_NAME')
-AWS_S3_CUSTOM_DOMAIN = getenv('AWS_S3_CUSTOM_DOMAIN')
-AWS_S3_URL_PROTOCOL = 'https:'
-
-AWS_HEADERS = {
-    'Expires': 'Thu, 1 Jan 2030 20:00:00 GMT',
-    'Cache-Control': 'max-age=315360000',
-}
-
-ACCOUNT_DEFAULT_HTTP_PROTOCOL = 'https'
